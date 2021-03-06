f4a8dfecd559e097312abbc5121f42262f195a19
MSA <msaxmsa@gmail.com>
2021-03-06 23:05:54 +0330

    Split Id and Symbol from Html code

--- Download_CSV.py
+++ Download_CSV.py
@@ -1,5 +1,7 @@
 # import lib
 from requests import get
+from bs4 import BeautifulSoup
+import re
 
 
 class CSVFile:
@@ -12,3 +14,26 @@ class CSVFile:
         # get content
         content = response.text
         return content
+    
+    @staticmethod
+    def __Split_Id_Symbol(content):
+        soup = BeautifulSoup(content, 'html.parser')
+        # select table
+        div = soup.findAll('div', attrs={'class': 'content'})
+        # split rows
+        tr = div[0].find_all('tr')
+        _dict = dict()
+        for i in tr[1:]:
+            # split Columns
+            td = i.find_all('td')
+            # select symbol Column
+            td_6 = td[6]
+            # find symbol
+            txt = td_6.text
+            # find href
+            href = td_6.find('a')['href']
+            # find id
+            _id = re.findall(r'inscode=([\d]+)', href)[0]
+            # add symbol and id to dict
+            _dict[txt] = _id
+        return _dict
